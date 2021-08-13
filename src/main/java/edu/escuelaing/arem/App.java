9ef6d78b1d422176a5888f838b171d9b1d507d6c
Juan Manuel Herrera Moya <juan.herrera-m@mail.escuelaing.edu.co>
2021-08-12 22:10:26 -0400

    Running app whit Spark Java

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -1,4 +1,5 @@
 package edu.escuelaing.arem;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -8,6 +9,7 @@ public class App
 {
     public static void  main(String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Starting!" );
+        get("/hello", (req, res) -> "Hello World");
     }
 }
