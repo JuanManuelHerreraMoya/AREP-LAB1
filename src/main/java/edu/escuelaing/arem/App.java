37f312942ba43c4b08fabc004e276f86d178e821
Juan Manuel Herrera Moya <juan.herrera-m@mail.escuelaing.edu.co>
2021-08-16 20:51:57 -0400

    Avance Heroku Local

--- src/main/java/edu/escuelaing/arem/App.java
+++ src/main/java/edu/escuelaing/arem/App.java
@@ -9,7 +9,13 @@ public class App
 {
     public static void  main(String[] args )
     {
-        System.out.println( "Starting!" );
-        get("/hello", (req, res) -> "Hello World");
+        port(getPort());
+        get("/hello", (req, res) -> "Hello Heroku");
+    }
+    static int getPort(){
+        if (System.getenv("PORT")!= null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;// return default port if heroku-port ins't set (on local host)
     }
 }
